527769f79ae528a6c1d53adca2c2b072601b357b
HardSt <hardstonepaul@hotmail.com>
2022-10-28 10:02:59 -0500

    update and purge two image folders

--- update-img-server.py
+++ update-img-server.py
@@ -3,8 +3,6 @@
 #/home/g0esferos/public_html/site/img/p
 #catalogo
 #/home/c4t4l0g0esp/public_html/img/p
-#ohquecompra
-#/home/iejj8sumklhb/public_html/img/p
 
 from dirsync import sync
 esferos = '/home/g0esferos/public_html/site/img/p'
@@ -12,9 +10,9 @@ catalogo = '/home/c4t4l0g0esp/public_html/img/p'
 sync(esferos, catalogo, 'sync', purge = True)
 
 
-#----- para restablecer los permisos de la carpeta
+#----- to restore folder permissions
 #sudo chmod 755 -R /home/c4t4l0g0esp/public_html/img
 #sudo chmod 755 -R /home/iejj8sumklhb/public_html/img
 
-#para pasra todas las carpetas a 755
+# put all folders in a 755
 #find /opt/lampp/htdocs -type d -exec chmod 755 {} \;
